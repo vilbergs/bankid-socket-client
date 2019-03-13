7191909ded1bc24e334b8a05c58463f5850959bb
Vilberg Eliasson <vilbergs@gmail.com>
2019-03-13 14:35:52 +0100

    Remove css, add state and socket listeners

--- src/App.js
+++ src/App.js
@@ -1,76 +1,80 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
-import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 
-const styles = {
-    root: {
-        flexGrow: 1,
-        marginTop: 50,
-    },
-    card: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        fontSize: 8,
-        marginBottom: 12,
-    },
-}
+import io from 'socket.io-client'
 
-function SimpleCard(props) {
-    const { classes } = props
-    const bull = <span className={classes.bullet}>•</span>
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pnr: '',
+            connected: false,
+            authenticated: false,
+        }
+    }
+    socket
 
-    return (
-        <div className={classes.root}>
+    componentDidMount() {
+        this.socket = io('172.17.4.231:5000')
+        this.socket.on('connect', () => this.setState({ connected: true }))
+        this.socket.on('success', () => this.setState({ authenticated: true }))
+    }
+
+    handleChange = (event) => {
+        this.setState({ pnr: event.target.value })
+    }
+
+    handleClick = (e) => {
+        this.socket.emit('authenticateAndCollect', this.state.pnr)
+    }
+
+    render() {
+        return (
             <Grid container spacing={24} justify="center">
                 <Grid item xs={4}>
-                    <Card className={classes.card}>
+                    <Card>
                         <CardContent>
                             <Typography variant="h5" component="h2">
                                 Signera med BankID
                             </Typography>
+                            <Typography
+                                component="p"
+                                style={{
+                                    color: this.state.connected
+                                        ? 'green'
+                                        : 'red',
+                                }}
+                            >
+                                ****
+                            </Typography>
                             <TextField
+                                value={this.state.pnr}
+                                onChange={this.handleChange}
                                 id="standard-name"
                                 label="Personnummer"
-                                className={classes.textField}
                                 margin="normal"
                             />
-                            <Typography
-                                className={classes.pos}
-                                color="textSecondary"
-                            >
-                                YYMMDDNNNN
-                            </Typography>
                         </CardContent>
                         <CardActions>
-                            <Button color="primary" size="small">
+                            <Button
+                                onClick={this.handleClick}
+                                color="primary"
+                                size="small"
+                            >
                                 Start
                             </Button>
                         </CardActions>
                     </Card>
                 </Grid>
             </Grid>
-        </div>
-    )
+        )
+    }
 }
 
-SimpleCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(SimpleCard)
+export default App
